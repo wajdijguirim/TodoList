951db2f50a9a1af2b4b769db563113b758ff8574
JGUIRIM <wajdijguirim@gmail.com>
2019-05-04 10:44:52 +0200

    Add crude methods and search bar future :D

---
+++ TodoList/Controllers/ToDoListViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by PAUL on 25/04/2019.
+//  Copyright © 2019 WJGUIRIM. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ToDoListViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        loadItems()
+        
+    }
+    // MARK - tableview Data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
+        
+        return cell
+
+    }
+    
+    // MARK - tableView Delegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    //MARK - Add item
+    @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add new todo", style: .default) { (action) in
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            self.itemArray.append(newItem)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create a new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    // MArk save items
+    func saveItems(){
+        
+        do{
+            try context.save()
+        } catch{
+            print("Error saving context \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()){
+        
+        do{
+            itemArray  = try context.fetch(request)
+        }catch{
+            print("error fetching data\(error)")
+        }
+        tableView.reloadData()
+    }
+    
+}
+// MARK search bar methods
+
+extension ToDoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
+
